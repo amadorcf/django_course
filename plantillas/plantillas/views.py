594c71f40be14e3c9bb850de11a23b054b7b0ef9
amadorcf <amadorcf@outlook.com>
2022-09-28 18:51:05 +0200

    Initial commit

---
+++ plantillas/plantillas/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def simple(request):
+    return render(request, 'simple.html', {})
+
+
+def dinamico(request, name):
+    categories = ['code', 'design', 'HTML']
+    context = {'name': name, 'categories': categories}
+    return render(request, 'dinamico.html', context)
